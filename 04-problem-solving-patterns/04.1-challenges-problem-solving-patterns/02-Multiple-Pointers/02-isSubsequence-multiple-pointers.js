c911223397676fd82a3d8d20d06e0219c56e6177
Leon Low <leonlow@email.com>
2022-09-26 00:23:37 +0800

    Complete Multiple Pointers challenge

--- 04-problem-solving-patterns/04.1-challenges-problem-solving-patterns/02-Multiple-Pointers/02-isSubsequence-multiple-pointers.js
+++ 04-problem-solving-patterns/04.1-challenges-problem-solving-patterns/02-Multiple-Pointers/02-isSubsequence-multiple-pointers.js
@@ -4,7 +4,7 @@
 // somewhere in the second string, without their order changing.
 
 /* Your solution MUST have AT LEAST the following complexities:
-Time Complexity - O(N + M)
+Time Complexity - O(N + M))
 Space Complexity - O(1) */
 
 // isSubsequence('hello', 'hello world'); // true
@@ -13,5 +13,24 @@ Space Complexity - O(1) */
 // isSubsequence('abc', 'acb'); // false (order matters)
 
 function isSubsequence(str1, str2) {
-    
+    let index1 = 0
+    let index2 = 0
+    if (str1.length === 0) {
+        return true
+    }
+    while (index2 < str2.length) {
+        if (str1[index1] === str2[index2]) {
+            index1++
+        }
+        if (index1 === str1.length) {
+            return true
+        }
+        index2++
+    }
+    return false
 }
+
+console.log(isSubsequence('hello', 'hello world')); // true
+console.log(isSubsequence('sing', 'sting')); // true
+console.log(isSubsequence('abc', 'abracadabra')); // true
+console.log(isSubsequence('abc', 'acb')) // false
